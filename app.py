1429ee148879f76bc3364f0b3a03a402bc030167
Rietesh <rietesh4535@iitg.ac.in>
2019-04-19 14:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def upload_file():
     '''
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='127.0.0.1', port=port)
 
     #sess.init_app(app)
     #app.run(debug=True)
